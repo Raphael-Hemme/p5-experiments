986decc1833e88766b48e7711473f950c4f00d4c
Raphael Hemme <hemme.raphael@gmail.com>
2021-10-25 22:01:16 +0200

    Further improve visual appearance of lichen sketch

--- lichen-experiment_01/sketch.js
+++ lichen-experiment_01/sketch.js
@@ -15,12 +15,16 @@ let dots5 = [];
 let dots6 = [];
 let dots7 = [];
 
+let dots8 = [];
+
+let dots9 = [];
+
 
 function setup() {
   createCanvas(600, 600);
   dots = makeDots(200, 100);
   for (let el of dots) {
-    const intSubDotArr = makeSubDots(el.x, el.y, 20, 20)
+    const intSubDotArr = makeSubDots(el.x, el.y, 10, 20)
     dots2.push(...intSubDotArr);
   }
   for (let el of dots2) {
@@ -28,84 +32,89 @@ function setup() {
     dots3.push(...intSubDotArr);
   } 
   
-  dots4 = makeDots(400, 30);
+  dots4 = makeDots(50, 30);
+  dots4.forEach(el => el.size = random(1, 8))
   for (let el of dots4) {
-    const intSubDotArr = makeSubDots(el.x, el.y, 10, 10)
+    const intSubDotArr = makeSubDots(el.x, el.y, 30, 10)
     dots5.push(...intSubDotArr);
   }
+  dots5.forEach(el => el.size = random(3, 15))
 
-  dots6 = makeDots(200, 70);
+  dots6 = makeDots(300, 70);
   for (let el of dots6) {
-    const intSubDotArr = makeSubDots(el.x, el.y, 30, 15)
+    const intSubDotArr = makeSubDots(el.x, el.y, 10, 15)
     dots7.push(...intSubDotArr);
   }
+
+  dots8 = makeDots(300, 140);
+  dots8.forEach(el => el.size = random(1, 5))
+
+  dots9 = makeDots(200, 10);
+  dots9.forEach(el => el.size = random(1, 9))
+
 }
 
 function draw() {
   background(100);
+  colorMode('RGBA')
 
-  for (var i = 0; i < dots2.length; i++){
-    // strokeWeight(6);
-    // stroke(163, 118, 21)
+  for (var i = 0; i < dots8.length; i++){
     fill(163, 118, 21);
     noStroke();
-    circle(dots2[i].x, dots2[i].y, 6);
+    circle(dots8[i].x, dots8[i].y, dots8[i].size);
+  }
+
+  for (var i = 0; i < dots2.length; i++){
+    fill(163, 118, 21);
+    noStroke();
+    circle(dots2[i].x, dots2[i].y, 20);
   }
   for (var i = 0; i < dots.length; i++){
-    // strokeWeight(9);
-    // stroke(135, 87, 14)
-    // noStroke();
     fill(135, 87, 14);
     stroke(110, 86, 36)
     strokeWeight(1);
     circle(dots[i].x, dots[i].y, 9);
   }
   for (var i = 0; i < dots3.length; i++){
-/*     strokeWeight(5);
-    stroke(196, 152, 39)
-    point(dots3[i].x, dots3[i].y); */
     strokeWeight(1);
     fill(196, 152, 39)
     stroke(133, 91, 19)
-    // noStroke();
     circle(dots3[i].x, dots3[i].y, 8);
   }
-  noFill();
-  noStroke();
-
-    // draw dots5 and dots4
-    for (var i = 0; i < dots5.length; i++){
-      // strokeWeight(6);
-      // stroke(133, 91, 19)
-      fill(133, 91, 19);
-      noStroke();
-      circle(dots5[i].x, dots5[i].y, 9);
-    }
-    for (var i = 0; i < dots4.length; i++){
-      // strokeWeight(2);
-      // stroke(112, 82, 29)
-      fill(112, 82, 29);
-      noStroke();
-      circle(dots4[i].x, dots4[i].y, 2);
-    }
 
   // draw dots6 and dots7
   for (var i = 0; i < dots6.length; i++){
-    // strokeWeight(1);
-    // stroke(133, 91, 19)
-    fill(196, 152, 39);
+    fill(189, 145, 32);
     noStroke();
-    circle(dots6[i].x, dots6[i].y, 8);
+    circle(dots6[i].x, dots6[i].y, 10);
   }
-  noFill();
   for (var i = 0; i < dots7.length; i++){
-    noStroke();
-    // stroke(112, 82, 29)
-    // strokeWeight(1);
-    // stroke(169, 131, 35);
+    // noStroke();
+    stroke(161, 125, 33, 90)
     fill(179, 139, 37)
     circle(dots7[i].x, dots7[i].y, 9);
   }
+
+
+  // draw dots5 and dots4
+  for (var i = 0; i < dots5.length; i++){
+    fill(133, 91, 19);
+    noStroke();
+    circle(dots5[i].x, dots5[i].y, dots5[i].size);
+  }
+  for (var i = 0; i < dots4.length; i++){
+    fill(174, 134, 36);
+    noStroke();
+    circle(dots4[i].x, dots4[i].y, dots4[i].size);
+  }
+
+  // draw dots9
+  for (var i = 0; i < dots9.length; i++){
+    fill(107, 76, 33);
+    noStroke();
+    circle(dots9[i].x, dots9[i].y, dots9[i].size);
+  }
+
 
   strokeWeight(1);
   noFill();
