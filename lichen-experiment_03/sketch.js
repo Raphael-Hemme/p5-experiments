b536f3fd3a8d8960d0edb9cb00a0770ca889ce40
Raphael Hemme <hemme.raphael@gmail.com>
2021-11-03 08:58:45 +0100

    Refactor cell generation in lichen-experiment_03

--- lichen-experiment_03/sketch.js
+++ lichen-experiment_03/sketch.js
@@ -9,17 +9,34 @@ let r = [];
 let g = [];
 let b = [];
 
+class Cell {
+  constructor(x, y, size, xSpeed, ySpeed, traveledDist, r, g, b) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.xSpeed = xSpeed;
+    this.ySpeed = ySpeed;
+    this.traveledDist = traveledDist;
+    this.r = r;
+    this.g = g;
+    this.b = b;
+  }
+}
+
+const cells = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  generateCells(3, 30);
+  generateCells(100, 3, 30);
+  console.log(cells)
 }
 
 function draw() {
   background(0, 50);
   
-  moveCells(300)
+  // moveCells(300)
+  
 }
 
 function moveCells(maxDist) {
@@ -43,8 +60,30 @@ function moveCells(maxDist) {
   }
 }
 
-function generateCells(minSize, maxSize) {
-  for(let i = 0; i < ballCount; i++) {
+function generateCells(amount, minSize, maxSize) {
+  const internalCellArr = [];
+  for(let i = 0; i < amount; i++) {
+
+    const x = width / 2; 
+    const y = height / 2; 
+
+    const xSpeed = random(-5, 5);
+    const ySpeed = random(-5, 5);
+
+    const traveledDist = 0;
+    const size = random(minSize, maxSize);
+
+    const r = random(0, 256);
+    const g = random(0, 256);
+    const b = random(0, 256);
+
+    internalCellArr.push(new Cell(x, y, size, xSpeed, ySpeed, traveledDist, r, g, b))
+  }
+  cells.push(internalCellArr);
+}
+
+/* function generateCells(amount, minSize, maxSize) {
+  for(let i = 0; i < amount; i++) {
 
     x[i] = width / 2; 
     y[i] = height / 2; 
@@ -60,4 +99,5 @@ function generateCells(minSize, maxSize) {
     g[i] = random(0, 256);
     b[i] = random(0, 256);
   }
-}
+} */
+
