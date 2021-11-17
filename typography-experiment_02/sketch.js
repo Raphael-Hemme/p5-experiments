35cba6f9c5dae868f986286f9d6a05a3aac54271
Raphael Hemme <hemme.raphael@gmail.com>
2021-11-17 21:24:43 +0100

    Change shape generation in typography-experiment_02 sketch

--- typography-experiment_02/sketch.js
+++ typography-experiment_02/sketch.js
@@ -1,30 +1,30 @@
 const chars = {
-  a: [],
-  b: [],
-  c: [],
-  d: [],
-  e: [],
-  f: [],
-  g: [],
-  h: [],
-  i: [],
-  j: [],
-  k: [],
-  l: [],
-  m: [],
-  n: [],
-  o: [],
-  p: [],
-  q: [],
-  r: [],
-  s: [],
-  t: [],
-  u: [],
-  v: [],
-  w: [],
-  x: [],
-  y: [],
-  z: [],
+  a: {},
+  b: {},
+  c: {},
+  d: {},
+  e: {},
+  f: {},
+  g: {},
+  h: {},
+  i: {},
+  j: {},
+  k: {},
+  l: {},
+  m: {},
+  n: {},
+  o: {},
+  p: {},
+  q: {},
+  r: {},
+  s: {},
+  t: {},
+  u: {},
+  v: {},
+  w: {},
+  x: {},
+  y: {},
+  z: {},
   space: 'space',
   return: 'return'
 }
@@ -33,6 +33,29 @@ let ioStringArr = [];
 
 let yOffset = 100;
 let counter = 0;
+
+class DotTriplet {
+  constructor(x1, y1) {
+    this.firstDot.x = x1;
+    this.firstDot.y = y1;
+    this.secondDot.x = x1 + 10;
+    this.secondDot.y = y1;
+    this.thirdDot.x = x1;
+    this.thirdDot.y = y1 + 10;
+  }
+  firstDot = {
+    x: 0,
+    y: 0
+  };
+  secondDot = {
+    x: 0,
+    y: 0
+  };
+  thirdDot = {
+    x: 0,
+    y: 0
+  };
+}
 
 function setup() {
   generateAlphabet();
@@ -51,8 +74,37 @@ function draw() {
   drawString(ioStringArr);
 }
 
+function generateHorizontalBarArr(n) {
+  const result = [];
+  for(let i = 0; i < n; i++) {
+    const currX = int(random(0, 80));
+    result.push({
+      x: currX,
+      y: int(random(0, 90)),
+      w: int(random(10, 100 - currX)),
+      h: 5
+    });
+  };
+  return result;
+}
+
+function generateVerticalBarArr(n) {
+  const result = [];
+  for(let i = 0; i < n; i++) {
+    const currY = int(random(0, 80));
+    result.push({
+      x: int(random(0, 90)),
+      y: currY,
+      w: 5,
+      h: int(random(10, 100 - currY))
+    });
+  };
+  return result;
+}
+
 function generateLetter() {
-  const pointCount = random(5, 10)
+  // Old shape-data generation:
+  /*   const pointCount = random(5, 10)
   const points = [];
   for (let i = 0; i < pointCount; i++) {
     points.push({
@@ -60,7 +112,17 @@ function generateLetter() {
       y: random(5, 95)
     })
   }
-  return points;
+  return points; */
+
+  // New shape-data generation:
+  const horizontalBarCount = random(0, 5);
+  const dotTriplet = new DotTriplet(int(random(10, 91)), int(random(10, 91)));
+  const verticalBarCount = random(0, 5);
+  return {
+    horizontalBarArr: generateHorizontalBarArr(horizontalBarCount),
+    verticalBarArr: generateVerticalBarArr(verticalBarCount),
+    dotTriplet: dotTriplet
+  }
 }
 
 function generateAlphabet() {
@@ -70,17 +132,47 @@ function generateAlphabet() {
       continue;
     };
     const currLetter = generateLetter()
-    chars[char].push(...currLetter)
+    chars[char] = currLetter;
   }
 }
 
 function drawChar(charPointArr) {
-  beginShape();
+  // old drawing function:
+  /* beginShape();
     for (let point of charPointArr) {
       vertex(point.x, point.y);
     } 
     vertex(charPointArr[0].x, charPointArr[0].y)
-  endShape();
+  endShape(); */
+
+  // new drawing function: charPointArr is not renamed to be less confusing if old function
+  // is restored. It is an object however.
+  for (let hbar of charPointArr.horizontalBarArr) {
+    rect(hbar.x, hbar.y, hbar.w, hbar.h);
+  }
+  for (let vbar of charPointArr.verticalBarArr) {
+    rect(vbar.x, vbar.y, vbar.w, vbar.h);
+  }
+  for (let dot in charPointArr.dotTriplet) {
+    // console.log(dot)
+    // circle(charPointArr[dot].x, charPointArr[dot].y, charPointArr[dot].w, charPointArr[dot].h)
+  }
+  circle(
+    charPointArr.dotTriplet.firstDot.x,
+    charPointArr.dotTriplet.firstDot.y,
+    5
+  );
+  circle(
+    charPointArr.dotTriplet.secondDot.x,
+    charPointArr.dotTriplet.secondDot.y,
+    5
+  )
+  circle(
+    charPointArr.dotTriplet.thirdDot.x,
+    charPointArr.dotTriplet.thirdDot.y,
+    5
+  )
+
 };
 
 function drawString(stringArr) {
@@ -130,7 +222,7 @@ function keyTyped() {
     ioStringArr.push('return');
   } else {
     const currKey = key;
-    currKey.toLowerCase();
+    currKey.toLowerCase(); // ToDo: not working
     ioStringArr.push(currKey)
   }
 };
